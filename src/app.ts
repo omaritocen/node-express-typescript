641db78c0f2645712480b68f82738258ea83cb8f
omaritocen <omaralaaabdelkader@gmail.com>
2023-02-14 02:58:04 +0200

    Add formatting and Linting

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express, { Application, Request, Response } from 'express';
 
 const app: Application = express();
 
-const PORT: number = 3001;
+const PORT = 3001;
 
 app.use('/', (req: Request, res: Response): void => {
     res.send('Hello world!');
